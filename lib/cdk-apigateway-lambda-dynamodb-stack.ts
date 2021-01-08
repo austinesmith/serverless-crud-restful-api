d1916690c50ccdef9173afaa77670d13c805255a
Austin Smith <austinfosec@gmail.com>
2021-01-07 23:27:11 -0600

    provisioned aws resources

--- lib/cdk-apigateway-lambda-dynamodb-stack.ts
+++ lib/cdk-apigateway-lambda-dynamodb-stack.ts
@@ -18,28 +18,99 @@ export class CdkApigatewayLambdaDynamodbStack extends cdk.Stack {
 
     // provision Dynamodb table https://docs.aws.amazon.com/cdk/api/latest/docs/aws-dynamodb-readme.html
     const table = new dynamodb.Table(this, "MyDDB", {
-      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      partitionKey: { 
+        name: 'id', 
+        type: dynamodb.AttributeType.STRING
+      },
+      tableName: 'items',
+      removalPolicy: cdk.RemovalPolicy.DESTROY
     });
 
     // provision Lambda resource https://docs.aws.amazon.com/cdk/api/latest/docs/aws-lambda-readme.html
-    const ddbLambda = new lambda.Function(this, 'DynamodbLambdaHandler', {
+    /*
+      const ddbLambda = new lambda.Function(this, 'DynamodbLambdaHandler', {
       runtime: lambda.Runtime.NODEJS_12_X,
-      /* code: lambda.Code.fromAsset(path.join(__dirname, 'lambda-handler')), */
+      code: lambda.Code.fromAsset(path.join(__dirname, 'lambda-handler')), 
       code: lambda.Code.fromAsset('lambda-handler'),
       handler: 'function.handler',
       environment: {
         MYDDB_TABLE_NAME: table.tableName,
       },
     });
+    */
 
-    // update policy to allow lambda to access dynamo
-    table.grantReadWriteData(ddbLambda);
+    // create
+    const createLambda = new lambda.Function(this, 'createItem', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset('lambda-handler'),
+      handler: 'function.handler',
+      environment: {
+        MYDDB_TABLE_NAME: table.tableName,
+        PRIMARY_KEY: 'id'
+      },
+    });
+
+    // read
+    const readLambda = new lambda.Function(this, 'readItem', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset('lambda-handler'),
+      handler: 'function.handler',
+      environment: {
+        MYDDB_TABLE_NAME: table.tableName,
+        PRIMARY_KEY: 'id'
+      },
+    });
+
+    // update
+    const updateLambda = new lambda.Function(this, 'updateItem', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset('lambda-handler'),
+      handler: 'function.handler',
+      environment: {
+        MYDDB_TABLE_NAME: table.tableName,
+        PRIMARY_KEY: 'id'
+      },
+    });
+
+    // delete
+    const deleteLambda = new lambda.Function(this, 'deleteItem', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset('lambda-handler'),
+      handler: 'function.handler',
+      environment: {
+        MYDDB_TABLE_NAME: table.tableName,
+        PRIMARY_KEY: 'id'
+      },
+    });
+
+    // update permission policys
+    table.grantReadWriteData(createLambda);
+    table.grantReadWriteData(readLambda);
+    table.grantReadWriteData(updateLambda);
+    table.grantReadWriteData(deleteLambda);
 
     // provision API Gateway
-    const api = new apigw.RestApi(this, 'my-api');
+    const api = new apigw.RestApi(this, 'crud-api', {
+      restApiName: 'CRUD API'
+    });
 
-    // add the root
-    api.root.resourceForPath(API_ROOT).addMethod('GET', new apigw.LambdaIntegration(ddbLambda));
+    // add the roots to CRUD API
+    /* api.root.resourceForPath(API_ROOT).addMethod('GET', new apigw.LambdaIntegration(ddbLambda)); */
+    // create resource
+    const multipleItems = api.root.addResource('items');
+    const singleItem = multipleItems.addResource('{id}');
+      // C
+    const createLambdaIntegration = new apigw.LambdaIntegration(createLambda);
+    multipleItems.addMethod('POST', createLambdaIntegration);
+      // R
+    const readLambdaIntegration = new apigw.LambdaIntegration(readLambda);
+    singleItem.addMethod('GET', readLambdaIntegration);
+      // U
+    const updateLambdaIntegration = new apigw.LambdaIntegration(updateLambda);
+    singleItem.addMethod('PATCH', updateLambdaIntegration);
+      // D
+    const deleteLambdaIntegration = new apigw.LambdaIntegration(deleteLambda);
+    singleItem.addMethod('DELETE', deleteLambdaIntegration);
 
     // output
     new cdk.CfnOutput(this, 'API URL:', {

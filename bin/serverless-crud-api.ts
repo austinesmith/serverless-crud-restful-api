d9614cfa00dc50ccc30b2479e106020178aa7a18
Austin Smith <austinfosec@gmail.com>
2021-02-05 02:41:01 -0600

    cdk code cleanup

--- bin/serverless-crud-api.ts
+++ bin/serverless-crud-api.ts
@@ -2,5 +2,8 @@
 import * as cdk from '@aws-cdk/core';
 import { ServerlessCrudApiStack } from '../lib/serverless-crud-api-stack';
 
+// new cdk application initialized from cdk.json:app
 const app = new cdk.App();
+
+// new stack in cdk application
 new ServerlessCrudApiStack(app, 'ServerlessCrudApiStack');
--- lib/serverless-crud-api-stack.ts
+++ lib/serverless-crud-api-stack.ts
@@ -3,15 +3,22 @@ import * as lambda from '@aws-cdk/aws-lambda';
 import * as dynamodb from '@aws-cdk/aws-dynamodb';
 import * as apigw from '@aws-cdk/aws-apigateway';
 import * as r53 from '@aws-cdk/aws-route53';
-import { HostedZone } from '@aws-cdk/aws-route53';
-import { Certificate } from '@aws-cdk/aws-certificatemanager';
-import { BasePathMapping, DomainName } from '@aws-cdk/aws-apigateway';
+import * as acm from '@aws-cdk/aws-certificatemanager';
+
+// account specific variables
+const CUSTOM_DOMAIN_NAME = 'crud.austinesmith.com';
+const PROD_HOSTED_ZONE_ID = 'Z0782095OAXYIN3YIA1L';
+const ZONE_NAME = 'austinesmith.com';
+const ACM_CERTIFICATE_ARN = 'arn:aws:acm:us-east-1:055016422806:certificate/4ad6cc2d-2ae0-455a-8780-0898ab441528';
 
 export class ServerlessCrudApiStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // DYNAMODB 
+    /* * * * * * * * *
+    / D Y N A M O D B
+    * * * * * * * * */
+
       // provision Dynamodb table
     const table = new dynamodb.Table(this, "MyDDB", {
       partitionKey: { 
@@ -22,11 +29,15 @@ export class ServerlessCrudApiStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
-    // LAMBDA
+
+    /* * * * * * * 
+    / L A M B D A 
+    * * * * * * */
+
       // Create: POST <URL>/items
     const createLambda = new lambda.Function(this, 'createItem', {
       runtime: lambda.Runtime.NODEJS_12_X,
-      code: new lambda.AssetCode('./lambdas'),
+      code: lambda.Code.fromAsset('lambdas'),
       handler: 'create.handler',
       environment: {
         TABLE_NAME: table.tableName,
@@ -36,7 +47,7 @@ export class ServerlessCrudApiStack extends cdk.Stack {
       // Read: GET <URL>/items/{id}
     const readLambda = new lambda.Function(this, 'readItem', {
       runtime: lambda.Runtime.NODEJS_12_X,
-      code: new lambda.AssetCode('./lambdas'),
+      code: lambda.Code.fromAsset('lambdas'),
       handler: 'read.handler',
       environment: {
         TABLE_NAME: table.tableName,
@@ -46,7 +57,7 @@ export class ServerlessCrudApiStack extends cdk.Stack {
       // Update: PATCH <URL>/items/{id}
     const updateLambda = new lambda.Function(this, 'updateItem', {
       runtime: lambda.Runtime.NODEJS_12_X,
-      code: new lambda.AssetCode('./lambdas'),
+      code: lambda.Code.fromAsset('lambdas'),
       handler: 'update.handler',
       environment: {
         TABLE_NAME: table.tableName,
@@ -56,7 +67,7 @@ export class ServerlessCrudApiStack extends cdk.Stack {
       // Delete: DELETE <URL>/items/{id}
     const deleteLambda = new lambda.Function(this, 'deleteItem', {
       runtime: lambda.Runtime.NODEJS_12_X,
-      code: new lambda.AssetCode('./lambdas'),
+      code: lambda.Code.fromAsset('lambdas'),
       handler: 'delete.handler',
       environment: {
         TABLE_NAME: table.tableName,
@@ -66,22 +77,24 @@ export class ServerlessCrudApiStack extends cdk.Stack {
       // Read All: GET <URL>/items
     const readAllLambda = new lambda.Function(this, 'readAllItems', {
       runtime: lambda.Runtime.NODEJS_12_X,
-      code: new lambda.AssetCode('./lambdas'),
+      code: lambda.Code.fromAsset('lambdas'),
       handler: 'read-all.handler',
       environment: {
         TABLE_NAME: table.tableName
       },
     });
-
-    // PERMISSIONS
-      // update IAM roles/policys
+      // roles for lambda to access dynamodb table
     table.grantReadWriteData(createLambda);
     table.grantReadWriteData(readLambda);
     table.grantReadWriteData(updateLambda);
     table.grantReadWriteData(deleteLambda);
     table.grantReadWriteData(readAllLambda);
 
-    // API GATEWAY
+    
+    /* * * * * * * * * * * *
+    / A P I   G A T E W A Y
+    * * * * * * * * * * * */
+   
       // provision API Gateway
     const api = new apigw.RestApi(this, 'crud-api', {
       restApiName: 'CRUD API'
@@ -106,34 +119,32 @@ export class ServerlessCrudApiStack extends cdk.Stack {
     const readAllLambdaIntegration = new apigw.LambdaIntegration(readAllLambda);
     everyItem.addMethod('GET', readAllLambdaIntegration);
 
-    // api.url = rest api URL
-    // create domain for api
-    const CUSTOM_DOMAIN_NAME = 'crud.austinesmith.com';
-    const PROD_HOSTED_ZONE_ID = 'Z0782095OAXYIN3YIA1L';
-    const ZONE_NAME = 'austinesmith.com';
-    const ACM_CERTIFICATE_ARN = 'arn:aws:acm:us-east-1:055016422806:certificate/4ad6cc2d-2ae0-455a-8780-0898ab441528';
-
-    // custom domain for api
-    const customDomain = new DomainName(this, 'customDomain', {
+    
+    /* * * * * * * *
+    / R O U T E 5 3
+    * * * * * * * */
+   
+      // custom domain for api
+    const customDomain = new apigw.DomainName(this, 'customDomain', {
       domainName: CUSTOM_DOMAIN_NAME,
-      certificate: Certificate.fromCertificateArn(this, 'ACM_Certificate', ACM_CERTIFICATE_ARN),
+      certificate: acm.Certificate.fromCertificateArn(this, 'ACM_Certificate', ACM_CERTIFICATE_ARN),
       endpointType: apigw.EndpointType.EDGE
     });
-    new BasePathMapping(this, 'CustomBasePathMapping', {
+      // map api to custom domain created above
+    new apigw.BasePathMapping(this, 'CustomBasePathMapping', {
       domainName: customDomain,
       restApi: api
     });
-    // Get a reference to AN EXISTING hosted zone using the HOSTED_ZONE_ID. You can get this from route53
-    const hostedZone = HostedZone.fromHostedZoneAttributes(this, 'HostedZone', {
+      // Get reference to existed hosted zone
+    const hostedZone = r53.HostedZone.fromHostedZoneAttributes(this, 'HostedZone', {
       hostedZoneId: PROD_HOSTED_ZONE_ID,
       zoneName: ZONE_NAME
     });
-    // Finally, add a CName record in the hosted zone with a value of the new custom domain that was created above:
+      // add CName record in the hosted zone with value of new custom domain created above
     new r53.CnameRecord(this, 'ApiGatewayRecordSet', {
       zone: hostedZone,
       recordName: 'api',
       domainName: customDomain.domainNameAliasDomainName
     });
-
   };
 }

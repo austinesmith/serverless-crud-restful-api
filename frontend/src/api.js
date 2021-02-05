4e4edb6136dadc6fbfa7130bcf256a4da94e4573
Austin Smith <austinfosec@gmail.com>
2021-02-05 07:01:04 -0600

    vue front end

---
+++ frontend/src/api.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+const client = axios.create({
+  baseURL: 'https://w497o4ibr7.execute-api.us-east-1.amazonaws.com/dev/',
+  json: true
+})
+
+export default {
+    async execute (method, resource, data) {
+        return client({
+            method,
+            url: resource,
+            data
+        }).then( req => {
+            return req.data
+        })
+    },
+    // CREATE
+    createItem ( data ) {
+        return this.execute( 'post' , 'items' , data )
+    },
+    // READ
+    getItem( id ) {
+        return this.execute( 'get' , 'items/${id}' )
+    },
+    // READ ALL
+    getItems() {
+        return this.execute( 'get' , 'items' )
+    },
+    // UPDATE
+    updateItem( id , data ) {
+        return this.execute( 'put' , 'items/${id}' , data )
+    },
+    // DELETE
+    deleteItem( id ) {
+        return this.execute( 'delete', 'items/${id}' )
+    }
+}
---
+++ frontend/src/components/ListView.vue
@@ -0,0 +1,17 @@
+<template>
+  <vec-fragment>
+    <td>{{ item.name }}</td>
+    <td>{{ item.surname.toUpperCase() }}</td>
+    <td>{{ item.age }}</td>
+    <td><b>{{ item.country }}</b></td>
+    <td>{{ item.club }}</td>
+  </vec-fragment>
+</template>
+
+<script>
+export default {
+  props: {
+    item: { type: Object, required: true, default: () => { } }
+  }
+}
+</script>
